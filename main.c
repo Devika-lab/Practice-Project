1747e08442250fc7541b1ce6c9fdedc2b315aa2d
Devika-lab <devikahoney55@gmail.com>
2025-10-28 18:21:49 +0530

    Add basic arithmetic functions and main function

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int add(int , int)
+int sub(int , int)
+int main()
+{
+  int a , b;
+  a = add(2,3);
+  printf("The output is %d",a);
+  b = sub(10,3);
+  printf("The output is %d",b);
+}
