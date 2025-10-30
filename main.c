50903fe4335f63c9c924c9d51be060b873f222fc
Devika <devikahoney55@gmail.com>
2025-10-30 07:23:52 +0000

    Added newline to main.c

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+#include"main.h"
 
 int main()
 {
   int a , b;
   a = add(2,3);
-  printf("The output is %d",a);
+  printf("The output of Add is %d",a);
   b = sub(10,3);
-  printf("The output is %d",b);
+  printf("The output of Sub is %d",b);
 }
