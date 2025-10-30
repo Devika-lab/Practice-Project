902232fb3559946d8f0c413c325899012887fb3a
Devika <devikahoney55@gmail.com>
2025-10-30 11:40:38 +0000

    Added division function

--- main.c
+++ main.c
@@ -3,11 +3,13 @@
 
 int main()
 {
-  int a , b , c;
+  int a , b , c,d;
   a = add(2,3);
   printf("The output of Add is %d\n",a);
   b = sub(10,3);
   printf("The output of Sub is %d\n",b);
-  c = sub(10,3);
+  c = mul(10,3);
   printf("The output of Mul is %d\n",b);
+  d = div(10,2);
+  printf("The output of div is %d\n",b);
 }
--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int div(int a,int b);
 int mul(int a,int b);
 int add(int , int);
 int sub(int , int);
